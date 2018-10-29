b2e60f8b306ae535feb5fee7e27ecff2b6998fde
mamudul-hassan <37290727+mamudul-hassan@users.noreply.github.com>
2018-10-29 23:08:54 +0600

    Update session.php

--- session.php
+++ session.php
@@ -27,8 +27,10 @@ if (intercept('POST')) {
 echo "hello world!" ;
 echo $path ;
 file_put_contents("php://stderr", "hello, this is a test!\n");
-
-
+$req_dump = print_r($_REQUEST, TRUE);
+$fp = fopen('request.log', 'a');
+fwrite($fp, $req_dump);
+fclose($fp);
 
 ?>
 
